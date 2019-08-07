278866c824b211b52cbc485455a2c9f5b7b4d1b6
CharlesTsaoBattle <92019797@hotmail.com>
2019-08-08 10:23:59 +1200

    Integrated polished bank and paymark pages

--- Coffee/Coffee/Pages/BankPage.xaml.cs
+++ Coffee/Coffee/Pages/BankPage.xaml.cs
@@ -6,57 +6,98 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Coffee.Models;
+using SQLite;
 
 namespace Coffee.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BankPage : ContentPage
     {
-        string bankSelected;
         
+        string bankSelected;
+        string pickerSelected;
+        double topupAmount = 0.00f;
+        Boolean topupSelected = false;
+
+        //public Customer temp;
+        //float balance = 5.00f;
 
         public BankPage()
         {
             InitializeComponent();
-
-            List<int> amounts = new List<int>();
-            amounts.Add(5);
-            amounts.Add(10);
-            amounts.Add(20);
-            amounts.Add(50);
-        }
-
-        void tapImage_Tapped(object sender, EventArgs e)
-        {
-            // handle the tap    
-            DisplayAlert("Alert", "This is an image button", "OK");
+            ResetBankButtonsOpacity();
+            picker.SelectedIndexChanged += this.PickerSelectedIndexChanged;
         }
 
         void ANZButtonClicked(object sender, EventArgs e)
         {
-            bankSelected = "ANZ";
-            label.Text = $"{bankSelected} selected";
-
+            ResetBankButtonsOpacity();
+            SetSelectedBank("ANZ", ANZ);
         }
         void ASBButtonClicked(object sender, EventArgs e)
         {
-            bankSelected = "ASB";
-            label.Text = $"{bankSelected} selected";
+            ResetBankButtonsOpacity();
+            SetSelectedBank("ASB", ASB);
         }
 
         void BNZButtonClicked(object sender, EventArgs e)
         {
-            bankSelected = "BNZ";
-            label.Text = $"{bankSelected} selected";
+            ResetBankButtonsOpacity();
+            SetSelectedBank("BNZ", BNZ);
         }
+
         void KiwibankButtonClicked(object sender, EventArgs e)
         {
-            bankSelected = "Kiwibank";
-            label.Text = $"{bankSelected} selected";
+            ResetBankButtonsOpacity();
+            SetSelectedBank("Kiwibank", Kiwibank);
         }
-        void PaymarkButtonClicked(object sender, EventArgs e)
+
+        async void PaymarkButtonClicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new PaymarkPage());
+            if (bankSelected == null)
+            {
+                await DisplayAlert("Warning", "Please select a bank provider!", "OK");
+            }
+
+            else if (topupSelected == false)
+            {
+                await DisplayAlert("Warning", "Please select a top-up amount!", "OK");
+            }
+        
+            else
+            {
+                await Navigation.PushAsync(new PaymarkPage(bankSelected, pickerSelected));
+            }
+        }
+
+        void ResetBankButtonsOpacity()
+        {
+            ANZ.Opacity = 0.25;
+            ASB.Opacity = 0.25;
+            BNZ.Opacity = 0.25;
+            Kiwibank.Opacity = 0.25;
+        }
+
+        void SetSelectedBank(String stringBank, ImageButton currentBank)
+        {
+            if (bankSelected == stringBank)
+            {
+                bankSelected = null;
+            }
+            else
+            {
+                bankSelected = stringBank;
+                currentBank.Opacity = 1;
+            }
+        }
+
+        void PickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Method call every time when picker selection changed.
+            pickerSelected = picker.SelectedItem.ToString(); // Retrieves selected item of picker
+            topupAmount = Convert.ToDouble(pickerSelected.Remove(0, 1)); // Converts the selected picker item to a float by removing the '$' sign
+            topupSelected = true;
         }
     }
 }
